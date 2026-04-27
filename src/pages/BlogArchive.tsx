220c080612d242ce9ffb464c96f68999c6dccd52
guruprasad4256 <152155635+guruprasad4256@users.noreply.github.com>
2026-04-27 15:31:14 +0530

    sdsf

--- src/pages/BlogArchive.tsx
+++ src/pages/BlogArchive.tsx
@@ -1,11 +1,6 @@
 import BlogArchiveComponent from "../components/sections/BlogArchive";
 import Header from "../components/sections/Header";
 
-/**
- * BlogArchivePage 
- * Modern React/Vite does not require 'import React'.
- * Unused 'FloatingOrb' removed to pass strict TS build checks.
- */
 const BlogArchivePage = () => {
   return (
     <div className="w-full min-h-screen bg-black">
