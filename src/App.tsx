495fda4c26958fe7577a9486646b8e8f45bee77b
guruprasad4256 <152155635+guruprasad4256@users.noreply.github.com>
2026-04-28 18:48:44 +0530

    updated link for meeting

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<ComingSoon />} />
-      <Route path="/book-meeting-with-sathwik-putta" element={<SathwikMeeting />} />
+      <Route path="/book-a-meeting" element={<SathwikMeeting />} />
       <Route path="/blogs" element={<BlogArchive />} />
       <Route path="/blogs/:slug" element={<BlogSingle />} />
       <Route path="*" element={<NotFound />} />
